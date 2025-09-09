11da7a80d40067e80ee1ae2deb239620607ca08d
= <=>
2025-09-09 17:32:55 -0400

    bindings: ts: Remove unused shmbus bindings; fully implement public API for Deno bindings & clean up exports

--- bindings/ts/splinter_deno_ffi.ts
+++ bindings/ts/splinter_deno_ffi.ts
@@ -15,13 +15,32 @@ export const Libsplinter = Deno.dlopen("../../libsplinter.so", {
     parameters: ["buffer", "usize", "usize"],
     result: "i32",
   },
-  "splinter_open": { parameters: ["buffer"], result: "i32" },
-  "splinter_set": { parameters: ["buffer", "pointer", "usize"], result: "i32" },
-  "splinter_unset": { parameters: ["buffer"], result: "i32" },
+  "splinter_open": { 
+    parameters: ["buffer"], 
+    result: "i32" 
+  },
+  "splinter_set": { 
+    parameters: ["buffer", "pointer", "usize"], 
+    result: "i32" 
+  },
+  "splinter_unset": { 
+    parameters: ["buffer"], 
+    result: "i32" 
+  },
   "splinter_get": {
     parameters: ["buffer", "pointer", "usize", "pointer"],
     result: "i32",
   },
-  "splinter_close": { parameters: [], result: "void" },
+  "splinter_list": { 
+    parameters: ["pointer", "usize", "pointer"], 
+    result: "i32" 
+  },
+  "splinter_poll": { 
+    parameters: ["buffer", "u64"], 
+    result: "i32" 
+  },
+  "splinter_close": { 
+    parameters: [], 
+    result: "void" 
+  }
 });
-
