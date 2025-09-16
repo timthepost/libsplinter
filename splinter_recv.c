7cceb20ad1ea2fd98afabead2da07f190794f001
= <=>
2025-09-16 14:03:42 -0400

    tools: refactor for standard posix correctness

--- splinter_recv.c
+++ splinter_recv.c
@@ -1,21 +1,35 @@
 /**
- * A very crude tool to block and wait for a key to update, then print it.
+ * Copyright 2025 Tim Post
+ * A tool to block and wait for a key to update, then print it.
  * Runs forever by default, --oneshot will cause it to exit after one event
  * fires.
- *
- * This needs to be proper-ized (--help, --version, short options, etc)
+ * License: MIT
  */
-#include "splinter.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <string.h>
+#include <getopt.h>
 
-#define DEFAULT_BUS "splinter_debug"
-#define DEFAULT_KEY "__debug"
+#include "splinter.h"
+#include "config.h"
+
 #define MAX_LEN 4096
 #define TIMEOUT_MS 100
+
+
+static void print_help(const char *program_name) {
+    printf("Usage: %s [OPTIONS]\n", program_name);
+    printf("\nListen for messages on a splinter bus key.\n");
+    printf("\nOptions:\n");
+    printf("  -o, --oneshot         Exit after receiving one message\n");
+    printf("  -h, --help            Show this help message\n");
+    printf("  -v, --version         Show version information\n");
+}
+
+static void print_version(void) {
+    printf("splinter-recv %s\n", SPLINTER_VERSION);
+}
 
 int main(int argc, char **argv) {
     int rc = 0, errlvl = 0;
@@ -23,8 +37,33 @@ int main(int argc, char **argv) {
     char msg[MAX_LEN];
     size_t msg_sz = 0;
     
-    if ((argc == 2) && (argv[1] != NULL) && (! strncmp(argv[1], "--oneshot", 9))) {
-        oneshot = 1;
+    static struct option long_options[] = {
+        {"oneshot", no_argument, 0, 'o'},
+        {"help",    no_argument, 0, 'h'},
+        {"version", no_argument, 0, 'v'},
+        {0, 0, 0, 0}
+    };
+    
+    int c;
+    while ((c = getopt_long(argc, argv, "oh?v", long_options, NULL)) != -1) {
+        switch (c) {
+            case 'o':
+                oneshot = 1;
+                break;
+            case 'h':
+            case '?':
+                print_help(argv[0]);
+                return 0;
+            case 'v':
+                print_version();
+                return 0;
+            default:
+                fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
+                return 1;
+        }
+    }
+    
+    if (oneshot) {
         fprintf(stderr, "splinter-recv: will exit after 1 event (--oneshot specified)\n");
     }
 
@@ -34,7 +73,9 @@ int main(int argc, char **argv) {
     }
 
     printf("splinter-recv: listening to %s on %s ...\n", DEFAULT_KEY, DEFAULT_BUS);
-    if (! oneshot) fprintf(stderr, "splinter-recv: use --oneshot if you ever wish to exit after a single event.\n");
+    if (!oneshot) {
+        fprintf(stderr, "splinter-recv: use --oneshot if you ever wish to exit after a single event.\n");
+    }
     
     do {
         rc = splinter_poll(DEFAULT_KEY, TIMEOUT_MS);
--- splinter_send.c
+++ splinter_send.c
@@ -1,35 +1,79 @@
 /**
- * A very crude tool to write to a key.
- *
- * This needs to be proper-ized (--help, --version, short options, etc)
+ * Copyright 2025 Tim Post
+ * A tool to write to a key (for whatever use)
+ * License: MIT
  */
-#include "splinter.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
-#define DEFAULT_BUS "splinter_debug"
-#define DEFAULT_KEY "__debug"
+#include "splinter.h"
+#include "config.h"
+
+static void print_help(const char *program_name) {
+    printf("Usage: %s [OPTIONS] [BUS] [KEY] VALUE\n", program_name);
+    printf("       %s [OPTIONS] [KEY] VALUE\n", program_name);
+    printf("       %s [OPTIONS] VALUE\n", program_name);
+    printf("\nSend a message to a splinter bus key.\n");
+    printf("\nArguments:\n");
+    printf("  BUS                    Bus name (default: %s)\n", DEFAULT_BUS);
+    printf("  KEY                    Key name (default: %s)\n", DEFAULT_KEY);
+    printf("  VALUE                  Message to send\n");
+    printf("\nOptions:\n");
+    printf("  -h, --help            Show this help message\n");
+    printf("  -v, --version         Show version information\n");
+}
+
+static void print_version(void) {
+    printf("splinter-send %s\n", SPLINTER_VERSION);
+}
 
 int main(int argc, char **argv) {
     const char *bus = DEFAULT_BUS;
     const char *key = DEFAULT_KEY;
-    const char *value;
-
-    if (argc < 2 || argc > 4) {
-        fprintf(stderr, "Usage: %s [bus_path] [key] value\n", argv[0]);
+    const char *value = NULL;
+    
+    static struct option long_options[] = {
+        {"help",    no_argument, 0, 'h'},
+        {"version", no_argument, 0, 'v'},
+        {0, 0, 0, 0}
+    };
+    
+    int c;
+    while ((c = getopt_long(argc, argv, "h?v", long_options, NULL)) != -1) {
+        switch (c) {
+            case 'h':
+            case '?':
+                print_help(argv[0]);
+                return 0;
+            case 'v':
+                print_version();
+                return 0;
+            default:
+                fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
+                return 1;
+        }
+    }
+    
+    /* Handle positional arguments */
+    int remaining_args = argc - optind;
+    
+    if (remaining_args < 1 || remaining_args > 3) {
+        fprintf(stderr, "splinter-send: invalid number of arguments\n");
+        fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
         return 1;
     }
 
-    if (argc == 2) {
-        value = argv[1];
-    } else if (argc == 3) {
-        key = argv[1];
-        value = argv[2];
-    } else {
-        bus = argv[1];
-        key = argv[2];
-        value = argv[3];
+    if (remaining_args == 1) {
+        value = argv[optind];
+    } else if (remaining_args == 2) {
+        key = argv[optind];
+        value = argv[optind + 1];
+    } else if (remaining_args == 3) {
+        bus = argv[optind];
+        key = argv[optind + 1];
+        value = argv[optind + 2];
     }
 
     if (splinter_open_or_create(bus, 128, 1024) != 0) {
