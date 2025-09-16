003e7fed91b2a6eaab3c1212d9e53792ad3c67d5
= <=>
2025-09-16 14:02:02 -0400

    build: centralize common constants in config.h

---
+++ config.h
@@ -0,0 +1,19 @@
+/**
+ * Miscellaneous Configuration Constants 
+ */
+#ifndef SPLINTER_CONFIG_H
+#define SPLINTER_CONFIG_H
+
+#define SPLINTER_VERSION "0.0.3"
+
+// so tools can be used without many / any arguments
+#define DEFAULT_BUS "splinter_debug"
+#define DEFAULT_KEY "__debug"
+
+// do we have the valgrind development headers?
+// comment out (undefine) if not.
+#define HAVE_VALGRIND_H 1
+
+// more can go here as needed.
+
+#endif // SPLINTER_CONFIG_H
