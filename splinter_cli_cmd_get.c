22531197c21d3e3040c2f60473a9b2647f1c0498
= <=>
2025-10-01 13:57:10 -0400

    cli: implement 'get'

--- splinter_cli_cmd_get.c
+++ splinter_cli_cmd_get.c
@@ -7,14 +7,30 @@ static const char *modname = "get";
 
 void help_cmd_get(unsigned int level) {
     (void) level;
+
     printf("%s gets the value of a key in the store.\n", modname);
     printf("Usage: %s <key_name>\n", modname);
     return;
 }
 
 int cmd_get(int argc, char *argv[]) {
-    (void) argc;
-    (void) argv;
+    char buf[4096] = { 0 };
+    size_t received = 0;
+    int rc = -1;
+
+    if (argc != 2) {
+        help_cmd_get(1);
+        return -1;
+    }
     
+    rc = splinter_get(argv[1], buf, sizeof(buf), &received);
+    if (rc != 0) {
+        fprintf(stderr, "%s: unable to retrieve key '%s'\n", modname, argv[1]);
+        return rc;
+    }
+
+    printf("%lu : %s\n", received, buf);
+    puts("");
+
     return 0;
 }
