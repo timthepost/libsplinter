1f8693b5246c6d6adbb1fb39116f5291b4baa785
= <=>
2025-10-01 15:55:27 -0400

    cli: have a basic 'list' now. MTC!

--- splinter_cli_cmd_list.c
+++ splinter_cli_cmd_list.c
@@ -5,16 +5,40 @@
 
 static const char *modname = "list";
 
+#define LIST_CMD_MAX_KEYS 2048
+
 void help_cmd_list(unsigned int level) {
     (void) level;
     printf("%s lists keys in the currently selected store.\n", modname);
-
+    printf("Usage: %s [pattern] [max_lines]\n", modname);
     return;
 }
+
+/*
+
+  // Test 13: List keys
+  char *key_list[10];
+  size_t key_count;
+  TEST("list keys", splinter_list(key_list, 10, &key_count) == 0);
+*/
 
 int cmd_list(int argc, char *argv[]) {
     (void) argc;
     (void) argv;
-    
-    return 0;
+
+    char *entries[LIST_CMD_MAX_KEYS] = { 0 };
+    size_t entry_count = 0;
+    int rc = -1, i;
+
+    rc = splinter_list(entries, LIST_CMD_MAX_KEYS, &entry_count);
+    if (rc == 0) {
+        for (i =0; entries[i]; i++) {
+            if (entries[i][0] != '\0')
+                printf(" %s\n", entries[i]);
+        }
+        puts("");
+        return 0;
+    }
+
+    return rc;
 }
