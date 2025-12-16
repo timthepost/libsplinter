b6fa3167ab5ca55551ebe3398f0e5eb09d12dc49
= <=>
2025-12-16 14:46:32 -0500

    build: add config options for default store geometry

--- config.h
+++ config.h
@@ -11,6 +11,8 @@
 // so tools can be used without many / any arguments
 #define DEFAULT_BUS "splinter_debug"
 #define DEFAULT_KEY "__debug"
+#define DEFAULT_SLOTS 1024
+#define DEFAULT_VAL_MAXLEN 4096
 
 // do we have the valgrind development headers?
 // comment out (undefine) if not.
