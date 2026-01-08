4404500d59a105e97f82884460cbbbe910d9dc13
= <=>
2026-01-08 11:59:11 -0500

    cli: watch: implement '--oneshot' watches, clean up code.

--- splinter_cli_cmd_watch.c
+++ splinter_cli_cmd_watch.c
@@ -50,7 +50,8 @@ void restore_terminal(void) {
 
 void help_cmd_watch(unsigned int level) {
     printf("%s watches a key in the current store for changes.\n", modname);
-    printf("Usage: %s <key_name_to_watch>\n", modname);
+    printf("Usage: %s <key_name_to_watch> [--oneshot]\n", modname);
+    puts("If --oneshot is specified, watch will exit after one event.");
     if (level) {
         puts("\nYou can also use the 'splinter_recv' program to poll in scripts.");
     }
@@ -60,20 +61,29 @@ void help_cmd_watch(unsigned int level) {
 int cmd_watch(int argc, char *argv[]) {
     char msg[4096];
     size_t msg_sz = 0;
-    char c, *key;
+    char c, *key = NULL;
     int rc = -1;
+    unsigned int oneshot = 0;
 
-    if (argc == 2) {
+    if (argc >= 2) {
         key = argv[1];
-    } else {
-        fprintf(stderr, "Usage: %s <key>\nTry 'help ext watch' for help.\n", modname);
+    }
+    
+    if (argc == 3 ) {
+        if (!strncasecmp(argv[2], "--oneshot", 10)) {
+            oneshot = 1;
+        } 
+    } 
+    
+    if (key == NULL) {
+        fprintf(stderr, "Usage: %s <key> [--oneshot]\nTry 'help ext watch' for help.\n", modname);
         return -1;
     }
-
-    // TODO: validate key prior to starting
     
     setup_terminal();
-    printf("Press Ctrl-] To Stop ...\n");
+
+    if (! oneshot)
+        puts("Press Ctrl-] To Stop ...");
     
     while (! thisuser.abort) {
         if (read(STDIN_FILENO, &c, 1) == 1) {
@@ -98,12 +108,18 @@ int cmd_watch(int argc, char *argv[]) {
                         key);
                 return -1;
             }
+            fprintf(stdout, "%lu:", msg_sz);
             fwrite(msg, 1, msg_sz, stdout);
             fputc('\n', stdout);
             fflush(stdout);
+            if (oneshot) {
+                // just raise it on behalf of the user since they specified it
+                thisuser.abort = 1;
+            }
         }
     }
 
+    puts(""); // GET ends with one blank line, so we emulate that here as well.
     thisuser.abort = 0;
     restore_terminal();
     
